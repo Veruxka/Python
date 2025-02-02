45b1600cabda088653fd9b70b4d5a180013eb4bc
Veruxka <bratkauskasveruxka@gmail.com>
2025-02-02 18:28:29 -0300

    Arquivo com o enunciado do programa boas vindas.

---
+++ boasvindas.py
@@ -0,0 +1,3 @@
+#Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas vindas para ela.
+#EX:.Qual é o seu nome? Fulano de Tal
+#EX:.Olá Fulano de Tal, é um prazer te conhecer!
