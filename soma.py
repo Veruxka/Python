693da1e29975e0b4d7d6a691e1fade3e0a30466b
Veruxka <bratkauskasveruxka@gmail.com>
2025-02-03 18:24:44 -0300

    Soma entre dois números inteiros.

---
+++ soma.py
@@ -0,0 +1,10 @@
+#Desenvolva um algoritmo que leia dois números inteiros e mostre o somatório entre eles.Ex:.
+#Digite um valor: 5
+#Digite outro valor: 4
+#A soma entre 5 e 4 é igual a 9.
+
+
+valor1 = int(input("Digite um valor: "))
+valor2 = int(input("Digite outro valor: "))
+soma = valor1 + valor2
+print(f"A soma entre {valor1} e {valor2} é igual a {soma}.")
