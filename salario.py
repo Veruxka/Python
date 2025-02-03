dba4b06aea8876be4596130b273ffd3ff4a2b9b0
Veruxka <bratkauskasveruxka@gmail.com>
2025-02-03 18:11:44 -0300

    Programa para ler o nome e o salário de um funcionário

---
+++ salario.py
@@ -0,0 +1,9 @@
+#Crie um programa que leia o nome de um funcionário, mostrando no final uma mensagem. Ex:.
+#Nome do funcionário: Fulano de Tal
+#Salário: R$ 1.000,00
+#O funcionário Fulano de Tal tem um salário de R$1.000,00 em junho
+
+nome = input("Nome do funcionário: ")
+salario = input("Salário: ")
+print(f"O funcionário {nome} tem um salário de {salario} em junho.")
+
