51ac3f0405de8db732a53a7bae4906a753b8d7e5
Veruxka <bratkauskasveruxka@gmail.com>
2025-02-03 17:44:23 -0300

    Esse é um programa que lê o nome da pessoa e da boas vindas.

--- boasvindas.py
+++ boasvindas.py
@@ -1,3 +1,5 @@
 #Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas vindas para ela.
 #EX:.Qual é o seu nome? Fulano de Tal
 #EX:.Olá Fulano de Tal, é um prazer te conhecer!
+nome = input("Qual é o seu nome?")
+print (f"Olá {nome}, é um prazer te conhecer!")
