d66e53fd1624c8df11b720f50d86ac3378858649
Veruxka <bratkauskasveruxka@gmail.com>
2025-02-03 19:11:23 -0300

    A média de um aluno.

---
+++ media.py
@@ -0,0 +1,9 @@
+#Faça um programa que leia as duas notas de um aluno em uma matéria e mostre na tela a sua média na disciplina. Ex:.
+#Nota 1: 4.5
+#Nota 2: 8.5
+#A média do aluno é 6.5
+
+nota1 = float(input("Digite a primeira nota:"))
+nota2 = float(input("Digite a segunda nota:"))
+media = (nota1 + nota2) /2
+print(f"A média do aluno é {media}.")
