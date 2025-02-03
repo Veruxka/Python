071471913f8096edf29f4c5497caef9ebd4e7eba
Veruxka <bratkauskasveruxka@gmail.com>
2025-02-03 19:36:43 -0300

    A terça parte de um número.

---
+++ umterço.py
@@ -0,0 +1,7 @@
+#Crie um algoritmo que leia um número real e mostre na tela a sua terça parte. Ex:.
+#Digite um número: 3.5
+#A terça parte de 3.5 é 1.16666.
+
+num = float(input("Digite um número:"))
+tparte = num / 3
+print (f"A terça parte de {num} é {tparte}.")
