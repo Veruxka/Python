e6ad2daeb469ff2ed4f64350f7799fa0bed47993
Veruxka <bratkauskasveruxka@gmail.com>
2025-02-03 19:30:27 -0300

    Dobro de um número.

---
+++ dobro.py
@@ -0,0 +1,7 @@
+#Crie um algoritmo que leia um numero real e mostre na tela o seu dobro. Ex:.
+#Digite um número: 3.5
+#O dobro de 3.5 é 7.
+
+num = float(input("Digite um número:"))
+dobro = num * 2
+print(f"O dobro de {num} é {dobro}.")
