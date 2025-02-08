023683170c6369ddf285cb6b44490b4b22d82da4
Veruxka <bratkauskasveruxka@gmail.com>
2025-02-07 22:04:32 -0300

    Verificando se o aluno ficou ou não acima da média.

---
+++ media_2.py
@@ -0,0 +1,14 @@
+#Crie um algoritmo que leia o nome e as duas notas de um aluno, calcule a sua média e mostre na tela.
+#No final, analise a média e mostre se o aluno teve ou não um bom aproveitamento (Se ficou acima da média 7.0)
+
+nome = (input("Qual é o seu nome?"))
+nota1 = float(input("Digite a primeira nota:"))
+nota2 = float(input("Digite a segunda nota"))
+media = (nota1 + nota2) /2
+
+if media >= 7.0:
+    print(f"{nome} você teve um bom aproveitamento.")
+
+else:
+    print(f"{nome} você não teve um bom aproveitamento.")
+
