3e8d29722bffc0284959dbbf61e439c555ab37ff
Veruxka <bratkauskasveruxka@gmail.com>
2025-02-12 16:09:50 -0300

    O quadrado de um número.

---
+++ quadrado.py
@@ -0,0 +1,7 @@
+#Efetuar a leitura de um valor numérico e apresentar o resultado do valor lido elevado ao quadrado.
+
+valor = int(input("Digite um valor:"))
+
+quadrado = valor * valor
+
+print(f"O quadrado de {valor} é {quadrado}")
